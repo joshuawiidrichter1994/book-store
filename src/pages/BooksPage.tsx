9f2432a360ff0880ffaac00a05d1ad78077d30f3
Joshua Richter <joshuawiidrichter@gmail.com>
2024-04-20 21:12:19 +0200

    Fixed routing / Imported books.json into BooksPage

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from 'react';
+// pages/BooksPage.tsx
+
+import React from 'react';
+import booksData from '../../public/data/books.json';
 
 const BooksPage = () => {
-    const [books, setBooks] = useState<{ title: string; author: string; price: number; }[]>([]);
-
-
-  useEffect(() => {
-    // Fetch data when component mounts
-    fetchData();
-  }, []);
-
-  const fetchData = () => {
-    // Here you would fetch data from the JSON file
-    // For demonstration purposes, using a dummy data
-    const dummyData = [
-      { title: 'Book 1', author: 'Author 1', price: 10.99 },
-      { title: 'Book 2', author: 'Author 2', price: 9.99 },
-      { title: 'Book 3', author: 'Author 3', price: 12.50 },
-    ];
-    setBooks(dummyData);
-  };
-
   return (
     <div className="container mx-auto mt-8">
       <h1 className="text-2xl font-bold mb-4">List of Books</h1>
       <ul>
-        {books.map((book, index) => (
+        {booksData.map((book, index) => (
           <li key={index} className="mb-4">
             <div className="font-bold">{book.title}</div>
             <div>Author: {book.author}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ export default function Home() {
           <div className="flex">
             {/* Navigation Links */}
             <div className="hidden md:flex md:ml-10 md:space-x-4">
-            <Link href="/books">
+            <Link href="/BooksPage">
               <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">Books</span>
             </Link>
-            <Link href="/add-book">
+            <Link href="/AddBookPage">
               <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">Add Book</span>
             </Link>
 
